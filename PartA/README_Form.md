70e20724a5318e843adae4febe2a3eb9a8e07e0f
Keyanshu007 <gariba.k@northeastern.edu>
2023-11-04 03:21:57 -0400

    second

---
+++ PartA/README_Form.md
@@ -0,0 +1,66 @@
+# Form Page README
+
+This README file provides an overview of the Form Page, its associated JavaScript, and CSS files.
+
+## Table of Contents
+
+1. [Introduction](#introduction)
+2. [File Structure](#file-structure)
+3. [HTML](#html)
+4. [JavaScript](#javascript)
+5. [CSS](#css)
+6. [Usage](#usage)
+7. [License](#license)
+
+## 1. Introduction
+
+The Form Page is a simple web page designed to collect user information through a form. It includes form fields for email, name, password, and password confirmation. The page uses JavaScript for real-time form validation and interactivity and is styled using CSS for a visually appealing user experience.
+
+## 2. File Structure
+
+- `index.html`: The HTML file that contains the structure of the web page and the form.
+- `form.js`: The JavaScript file responsible for form validation and interactivity.
+- `form.css`: The CSS file providing styles and layout for the web page.
+
+## 3. HTML (index.html)
+
+The `index.html` file is the main HTML document. It includes the following sections:
+
+- **Document Type and Language**: Defines the document type as HTML5 and specifies the language as English.
+- **Metadata**: Specifies the character encoding, viewport settings, and the page title.
+- **Container**: Wraps the content of the page within a container div.
+- **Form**: Contains the form with fields for email, name, password, and password confirmation.
+- **JavaScript and CSS**: Includes references to external JavaScript and CSS files.
+
+## 4. JavaScript (form.js)
+
+The `form.js` file contains JavaScript code for form validation and interactivity. Key features include:
+
+- Real-time validation of email, name, password, and password confirmation fields.
+- Enabling/disabling the "Login" button based on form field validity.
+- Handling the "Login" button click, redirecting users if all fields are valid.
+
+## 5. CSS (form.css)
+
+The `form.css` file provides styling and layout for the Form Page. It includes styles for:
+
+- Background color and font family for the entire page.
+- Container styles, including text alignment, margins, padding, background color, width, border radius, and box shadow.
+- Heading styles, adjusting font size, color, and margin.
+- Form, label, and input field styles for a clean and user-friendly appearance.
+- Styles for the "Login" button, including its appearance when disabled and enabled.
+
+## 6. Usage
+
+To use the Form Page, follow these steps:
+
+1. Clone the repository to your local environment.
+2. Open the `index.html` file in a web browser to view and interact with the form.
+
+## 7. License
+
+This project is licensed under the terms of the [MIT License](LICENSE).
+
+---
+
+Feel free to modify this README file to include any additional information, usage instructions, or licensing terms specific to your project.
--- PartB/stopwatch.js
+++ PartB/stopwatch.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const display = document.getElementById('Timer');
-    const datePicker = document.getElementById('date-picker');
-    const startBtn = document.getElementById('start');
-    const stopBtn = document.getElementById('stop');
-    const resetBtn = document.getElementById('reset');
-    let intervalId = null;
-    let elapsedTime = 0;
+    const Display = document.getElementById('Timer');
+    const dateMenu = document.getElementById('date-menu'); // Changed the id from 'date-picker' to 'date-menu' to match the HTML
+    const startButton = document.getElementById('start');
+    const stopButton = document.getElementById('stop');
+    const resetButton = document.getElementById('reset');
+    let intervalId = null; // Variable to store the interval ID for the stopwatch
+    let elapsedTime = 0; // Variable to track the elapsed time in milliseconds
 
     // Set current date in date picker and prevent typing
-    datePicker.valueAsDate = new Date();
-    datePicker.addEventListener('keydown', (e) => {
+    dateMenu.valueAsDate = new Date(); // Set the date picker to the current date
+    dateMenu.addEventListener('keydown', (e) => {
         e.preventDefault(); // Prevent typing in the date field
     });
 
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const seconds = Math.floor(elapsedTime / 1000) % 60;
         const minutes = Math.floor(elapsedTime / (1000 * 60)) % 60;
         const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-        display.textContent = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+        Display.textContent = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
     };
 
     // Pad numbers with leading zeros
@@ -30,18 +30,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
     // Start the stopwatch using async/await and Promises
-    startBtn.addEventListener('click', () => {
+    startButton.addEventListener('click', () => {
         if (intervalId !== null) return; // Prevent multiple intervals
         intervalId = setInterval(() => {
-            elapsedTime += 1000;
-            updateDisplay();
-        }, 1000);
+            elapsedTime += 1000; // Increment elapsed time by 1 second (1000 ms)
+            updateDisplay(); // Update the stopwatch display
+        }, 1000); // Update every 1 second (1000 ms)
     });
 
     // Stop the stopwatch using async/await
-    stopBtn.addEventListener('click', async () => {
+    stopButton.addEventListener('click', async () => {
         if (intervalId !== null) {
-            clearInterval(intervalId);
+            clearInterval(intervalId); // Clear the interval to stop the stopwatch
             intervalId = null;
             // An example of using async/await with a Promise
             // This does nothing functional here but shows how you can wait for a promise to resolve
@@ -50,13 +50,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Reset the stopwatch using async/await
-    resetBtn.addEventListener('click', async () => {
+    resetButton.addEventListener('click', async () => {
         if (intervalId !== null) {
-            clearInterval(intervalId);
+            clearInterval(intervalId); // Clear the interval to stop the stopwatch
             intervalId = null;
         }
-        elapsedTime = 0;
-        updateDisplay();
+        elapsedTime = 0; // Reset elapsed time to 0
+        updateDisplay(); // Update the display to show 00:00:00
         // An example of using async/await with a Promise
         // This does nothing functional here but shows how you can wait for a promise to resolve
         await delay(0);
